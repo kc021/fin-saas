ede82d60c384d38252b7a4fcff0eb53d934cfe26
kayo <kayodacosta8@gmail.com>
2026-01-29 17:00:34 -0300

    Fix: Implement and Export PrismaService correctly

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global() // <--- Deixa o banco disponível para o app todo
 @Module({
-  providers: [PrismaService]
+  providers: [PrismaService],
+  exports: [PrismaService], // <--- OBRIGATÓRIO: Permite que outros módulos usem o banco
 })
-export class PrismaModule {}
+export class PrismaModule { }
