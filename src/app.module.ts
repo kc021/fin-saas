f19f3688ca365b07749b9ec73ccb721be470f06c
kayo <kayodacosta8@gmail.com>
2026-01-29 17:16:52 -0300

    Fix: Register Auth and Transactions modules in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TransactionsModule } from './transactions/transactions.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [TransactionsModule, PrismaModule, AuthModule],
+  imports: [
+    // Carrega as variáveis de ambiente (.env)
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    // Nossos Módulos
+    PrismaModule,
+    AuthModule,
+    TransactionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
