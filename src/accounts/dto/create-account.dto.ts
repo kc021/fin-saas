73251a9d0cc35dca1214006490af529765c1b1b8
kayo <kayodacosta8@gmail.com>
2026-01-30 00:28:00 -0300

    Fix: Add properties to CreateAccountDto

--- src/accounts/dto/create-account.dto.ts
+++ src/accounts/dto/create-account.dto.ts
@@ -1,1 +1,22 @@
-export class CreateAccountDto {}
+import { IsNotEmpty, IsEnum, IsOptional, IsNumber } from 'class-validator';
+
+// Precisamos exportar o Enum para o Service usar também
+export enum AccountType {
+    CHECKING = 'CHECKING',
+    CASH = 'CASH',
+    SAVINGS = 'SAVINGS',
+    CREDIT = 'CREDIT',
+}
+
+export class CreateAccountDto {
+    @IsNotEmpty({ message: 'O nome da conta é obrigatório' })
+    name: string;
+
+    @IsEnum(AccountType, { message: 'Tipo de conta inválido' })
+    @IsOptional()
+    type?: AccountType;
+
+    @IsNumber({}, { message: 'O saldo deve ser um número' })
+    @IsOptional()
+    initialBalance?: number;
+}
