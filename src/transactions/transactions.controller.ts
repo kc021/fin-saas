9bdc348ea3af8002290291c1df46a62804f18bf8
kayo <kayodacosta8@gmail.com>
2026-01-30 00:10:49 -0300

    Fix: Update TransactionsController to pass tenantId

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,36 +1,37 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
-import { AuthGuard } from '@nestjs/passport'; // <--- O Porteiro do Passport
+import { AuthGuard } from '@nestjs/passport';
 
-@UseGuards(AuthGuard('jwt')) // <--- O CADEADO: Bloqueia tudo que estiver abaixo se não tiver login
+@UseGuards(AuthGuard('jwt'))
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) { }
 
   @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionsService.create(createTransactionDto);
+  create(@Body() createTransactionDto: CreateTransactionDto, @Req() req) {
+    // Pega o ID da empresa do token e manda pro serviço
+    return this.transactionsService.create(createTransactionDto, req.user.tenantId);
   }
 
   @Get()
-  findAll() {
-    return this.transactionsService.findAll();
+  findAll(@Req() req) {
+    return this.transactionsService.findAll(req.user.tenantId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.transactionsService.findOne(id);
+  findOne(@Param('id') id: string, @Req() req) {
+    return this.transactionsService.findOne(id, req.user.tenantId);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
-    return this.transactionsService.update(id, updateTransactionDto);
+  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto, @Req() req) {
+    return this.transactionsService.update(id, updateTransactionDto, req.user.tenantId);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.transactionsService.remove(id);
+  remove(@Param('id') id: string, @Req() req) {
+    return this.transactionsService.remove(id, req.user.tenantId);
   }
 }
