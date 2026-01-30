62e18e7beab9098f6e483f6fd5e0e585cec057e3
kayo <kayodacosta8@gmail.com>
2026-01-29 22:52:24 -0300

    Fix: Add CORS and ConfigModule force update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'; // <--- Importe isso
+import { ConfigModule } from '@nestjs/config'; // <--- Importante
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TransactionsModule } from './transactions/transactions.module';
@@ -8,7 +8,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), // <--- Adicione isso
+    ConfigModule.forRoot({ isGlobal: true }), // <--- Importante
     TransactionsModule,
     PrismaModule,
     AuthModule
