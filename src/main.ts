1c007ee0070bf7ab35a15ecbeeb5e27c46815d07
kayo <kayodacosta8@gmail.com>
2026-01-31 22:42:48 -0300

    Fix: Enable explicit CORS settings

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuração CORS "Blindada"
+  app.enableCors({
+    origin: true, // Permite qualquer origem (Frontend, Postman, etc)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Permite enviar o Token
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
